600e66d519c6d6fcafc6801dd7b0a927a5619c1c
김지웅 <godjw96@gmail.com>
2022-01-17 10:40:30 +0900

    Update OrderController.java

--- taco/src/main/java/springinaction/taco/web/OrderController.java
+++ taco/src/main/java/springinaction/taco/web/OrderController.java
@@ -1,5 +1,6 @@
 package springinaction.taco.web;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,29 +8,36 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import springinaction.taco.Order;
+import springinaction.taco.data.OrderRepository;
 
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@RequiredArgsConstructor
+@SessionAttributes("order")
 public class OrderController {
+
+    private final OrderRepository orderRepository;
 
 
     @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        orderRepository.save(order);
+        sessionStatus.setComplete();
 
-        log.info("Order submitted: " + order);
         return "redirect:/";
     }
 }
